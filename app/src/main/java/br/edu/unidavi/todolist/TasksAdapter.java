153d66f02a8a72560d7867872d6e1a39b3b6441a
DaviHoffmann <davi.phoffmann@gmail.com>
2018-10-06 08:50:38 -0300

    Projeto quase completo - primeiro commit

---
+++ app/src/main/java/br/edu/unidavi/todolist/TasksAdapter.java
@@ -0,0 +1,71 @@
+package br.edu.unidavi.todolist;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.ViewHolder> {
+
+    private final OnTaskClickListener listener;
+    private List<Task> tasks = new ArrayList<>();
+
+    public TasksAdapter(OnTaskClickListener listener) {
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+
+        return new ViewHolder(inflater.inflate(
+            android.R.layout.simple_list_item_1,
+                viewGroup,
+                false
+        ));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        final Task task =  tasks.get(i);
+        viewHolder.title.setText(task.getTitle());
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClick(task);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasks.size();
+    }
+
+    public void setup(List<Task> tasks) {
+        this.tasks.clear();
+        this.tasks.addAll(tasks);
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(android.R.id.text1);
+        }
+    }
+
+    interface OnTaskClickListener {
+        void onClick(Task task);
+    }
+
+}
